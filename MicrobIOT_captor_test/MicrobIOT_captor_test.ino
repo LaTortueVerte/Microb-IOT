7c063f986931c375b2cd0ceebab5387023c4ef7e
Robin DHEILLY <robin.dheilly1@gmail.com>
2022-06-20 12:17:27 +0200

    Sensor organization

--- MicrobIOT_captor_test/MicrobIOT_captor_test.ino
+++ MicrobIOT_captor_test/MicrobIOT_captor_test.ino
@@ -1,65 +1,76 @@
 #include "DHT.h"
-#define DHTPIN A4     // Digital pin connected to the DHT sensor
-const int ldrPin = A3;
-const int motorDCPin = 3;
+#define DHTPIN A4          // detection de la température et de l'humidité
+#define ldrPin A3          // detection de la luminosité
+#define motorDCPin 3       // actionneur moteur qui représente le ventilateur
+#define servmotorPIN 5     // actionneur moteur pour simuler l'ouverture/fermeture de la fenetre
+#define ledIncendiePIN 6   // actionneur LED Rouge activation d'un sytème qui éteint l'incendie
+#define ledDegEauxInPIN 4  // détecteur du niveau de l'eau dans le local
+#define ledDegEauxOutPIN 7 // actionneur LED Bleu coupure électricité des prises de la pièce
+#define mq9DOutPIN 8       // detection pin de sortie numérique du MQ9 pour les gaz
+#define mq9AOutPIN A2      // detection pin de sortie analogique du MQ9 pour les gaz
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+int readLevel;
+
+void setup()
+{
   Serial.begin(9600);
   dht.begin();
   pinMode(ldrPin, INPUT);
   pinMode(motorDCPin, OUTPUT);
+  pinMode(servmotorPIN, OUTPUT);
+  pinMode(ledIncendiePIN, OUTPUT);
+  pinMode(ledDegEauxInPIN, INPUT);
+  pinMode(ledDegEauxOutPIN, OUTPUT);
+  pinMode(mq9DOutPIN, OUTPUT);
+  pinMode(mq9AOutPIN, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
+  // Module qualité d'air
+  // si taux monoxide ou propane ou méthane, alerte utilisateur, ventilation + fenètre ouverte
+  // si température trop élevée à l'intérieur et ok à l'extérieur
+  // ouverture fenètre et ventilation
+  // else if température
   delay(2000);
-  //digitalWrite(motorDCPin, HIGH);
-  //delay(2000);
-  //digitalWrite(motorDCPin, LOW);
-  //Read humidity and temperature
+  // recupère les infos du DHT11 pour la température et l'humidité
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  //Print humidity and temperature
+  // Affichage de l'humidité et de la température sur le moniteur série
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.println(F("°C "));
 
-  //Action of the motor if the temperature is too high
-  /*if (t < 15) {
-     analogWrite(motorDCPin, 0);
-  }
-  else if (t >= 15 && t < 20){
-    analogWrite(motorDCPin, 70);
-  }
-  else if (t >=20 && t < 25){
-    analogWrite(motorDCPin, 150);
-  }
-  else{
-     analogWrite(motorDCPin, 255);
-  }*/
-  float Ftemp = (t-15)*(255/15);
+  // Le moteur s'active si la température est trop élevée
+  float Ftemp = (t - 15) * (255 / 15);
   int Pw = min(255, max(0, Ftemp));
   analogWrite(motorDCPin, Pw);
-  
 
-  //Test if it's bright or not
+  // Partie intrusion à utiliser sur FPGA
+  // Test la luminosité
   int ldrStatus = analogRead(ldrPin);
-  if (ldrStatus <= 50) {
-    Serial.print("Its DARK, Turn on the LED : ");
+  if (ldrStatus <= 50)
+  {
+    Serial.print("Ils fait sombre, allume la lumière ! luminosité : ");
     Serial.println(ldrStatus);
-  } 
-  else {
-    Serial.print("Its BRIGHT, Turn off the LED : ");
+  }
+  else
+  {
+    Serial.print("Ils fait claire, eteint la lumière ! luminosité : ");
     Serial.println(ldrStatus);
   }
 
-  
+  // Partie dégâts des eaux
+  readLevel = digitalRead(ledDegEauxInPIN);
 
-  
-
-
-  
+  // Partie incendie
+  // Si MQ9 detecte un incendie (à regarder comment)
+  digitalRead(ledIncendiePIN, HIGH);
+  Serial.println("Incendie détecté")
+      analogWrite(motorDCPin, 0); // switch off le ventilateur
+  // servo moteur ferme la porte
 }
